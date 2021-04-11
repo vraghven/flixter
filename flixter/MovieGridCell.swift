8995bff93760f08c2793dab4e056ce02ea6d0a6b
Vidya Raghvendra <vidyaraghvendra@c02dd085ml87.lan>
2021-04-11 16:31:46 -0700

    flixter pt 2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Vidya Raghvendra on 4/7/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
